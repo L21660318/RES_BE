7713fbe0c98425aa477b8de8ef60a6409f16b0ca
L21660318 <L21660318@matehuala.tecnm.mx>
2024-12-09 09:31:02 -0600

    raycuasa

--- app/Models/Proyecto.php
+++ app/Models/Proyecto.php
@@ -10,18 +10,8 @@ class Proyecto extends Model
     use HasFactory;
 
     protected $fillable = [
-<<<<<<< HEAD
         'nombre',
         'descripcion',
         'imagen'
     ];
-=======
-        'nombre', 'descripcion', 'imagen', 'usuario_id', 'archivo_pdf'
-    ];
-
-    public function usuario()
-    {
-        return $this->belongsTo(User::class);
-    }
->>>>>>> paginas-Diego
 }
